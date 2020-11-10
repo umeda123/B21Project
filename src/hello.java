c6525cb10f48b4ed45aacf9569aeb8c0914ab509
umeda Davlatova <umeda.davlatova@gmail.com>
2020-11-10 12:29:49 -0500

    initial commit

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,13 @@
 public class hello {
+    public static void main(String[] args) {
+        System.out.println("Mac user and windows user are happy to use commit");
+    }
     // more stuff
     // Mac user and windows user are happy to use commit
+
+
+
+
+
 
 }
